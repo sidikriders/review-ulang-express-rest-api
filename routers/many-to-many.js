4e225c5921e97d7985dad31e0f295265c9d32c41
sidikriders <sidik@xwork.id>
2018-03-29 09:57:49 +0700

    update routing for n-n and 1-n

--- routers/many-to-many.js
+++ routers/many-to-many.js
@@ -1,7 +1,84 @@
 var router = require('express').Router()
 
+var models = require('../models')
+var Group = models.group
+var Person = models.person
+var GroupPerson = models.group_person
+
 router.get('/', (req, res, next) => {
   res.status(200).send('try: /people or /groups')
 })
 
+router.get('/group', (req, res, next) => {
+  Group.findAll({
+    include: 'memberList'
+  })
+  .then(groups => res.status(200).send(groups))
+  .catch(err => {
+    console.log(err)
+    res.status(401).send(err)
+  })
+})
+
+router.post('/group', (req, res, next) => {
+  Group.create({
+    name: req.body.name
+  })
+  .then(group => res.status(200).send(group))
+  .catch(err => {
+    console.log(err)
+    res.status(401).send(err)
+  })
+})
+
+router.get('/person', (req, res, next) => {
+  Person.findAll({
+    include: ['group']
+  })
+  .then(groups => res.status(200).send(groups))
+  .catch(err => {
+    console.log(err)
+    res.status(401).send(err)
+  })
+})
+
+router.post('/person', (req, res, next) => {
+  if (req.body.group && req.body.group.length > 0) {
+    Person.create({
+      name: req.body.name
+    })
+    .then(person => {
+      console.log(person.id)
+      Promise.all(
+        req.body.group.map(_group => {
+          GroupPerson.create({
+            personId: person.id,
+            groupId: _group.id,
+            role: _group.role
+          })
+        })
+      )
+      .then(() => res.status(200).send('success'))
+      .catch(err => {
+        console.log(err)
+        res.status(401).send(err)
+      })
+    })
+    .catch(err => {
+      console.log(err)
+      res.status(401).send(err)
+    })
+  } else {
+    res.status(500).send('need group id')
+  }
+})
+
+router.get('/join', (req, res) => {
+  GroupPerson.findAll()
+  .then( data => res.send(data))
+  .catch(err => {
+    console.log(err)
+    res.send(err)
+  })
+})
 module.exports = router
--- routers/one-to-many.js
+++ routers/one-to-many.js
@@ -17,19 +17,19 @@ router.get('/batch', (req, res, next) => {
     }],
     attributes: ['id', 'name']
   })
-    .then(batches => res.status(200).send(batches))
-    .catch(err => {
-      console.log(err)
-      res.status(401).send({ msg: 'ERROR Sequelize', data: err })
-    })
+  .then(batches => res.status(200).send(batches))
+  .catch(err => {
+    console.log(err)
+    res.status(401).send({ msg: 'ERROR Sequelize', data: err })
+  })
 })
 
 router.post('/batch', (req, res, next) => {
   Batch.create({
     name: req.body.name
   })
-    .then(batch => res.status(200).send(batch))
-    .catch(err => res.status(401).send({ msg: 'ERROR Sequelize', data: err }))
+  .then(batch => res.status(200).send(batch))
+  .catch(err => res.status(401).send({ msg: 'ERROR Sequelize', data: err }))
 })
 
 router.get('/student', (req, res, next) => {
@@ -40,8 +40,8 @@ router.get('/student', (req, res, next) => {
     },
     attributes: ['id', 'name']
   })
-    .then(students => res.status(200).send(students))
-    .catch(err => res.status(401).send({ msg: 'ERROR Sequelize', data: err }))
+  .then(students => res.status(200).send(students))
+  .catch(err => res.status(401).send({ msg: 'ERROR Sequelize', data: err }))
 })
 
 router.post('/student', (req, res, next) => {
@@ -49,8 +49,8 @@ router.post('/student', (req, res, next) => {
     name: req.body.name,
     batchId: req.body.batchId
   })
-    .then(student => res.status(200).send(student))
-    .catch(err => res.status(401).send({ msg: 'ERROR Sequelize', data: err }))
+  .then(student => res.status(200).send(student))
+  .catch(err => res.status(401).send({ msg: 'ERROR Sequelize', data: err }))
 })
 
 module.exports = router
